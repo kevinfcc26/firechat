7e1252f7414aca8766eef266f6d83549de203e71
kevinfcc26 <kevinfcc26@gmail.com>
2020-07-01 17:39:58 -0500

    arerglo dependencias

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require( 'express' );
+const express = require('express');
+const path = require('path');
+
 const app = express();
-const path = require( 'path' );
 
-app.use( express.static( __dirname + '/dist' ));
+app.use(express.static(__dirname+'/dist/ng-blog'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+});
 
-app.listen( process.env.PORT || 8080 );
-
-//PathLocationStrategy
-
-app.get( '/*', function( req, res ){
-    res.sendFile( path.join( __dirname + '/dist/index.html' ));
-})
-
-console.log('Console listening!');
+app.listen(process.env.PORT || 8080);
