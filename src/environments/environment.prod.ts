d057684437035f564558168a8957fe8925b9a73f
kevinfcc26 <kevinfcc26@gmail.com>
2020-07-01 17:48:24 -0500

    firebase en production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDJgXiTaMSd5tS6pp2Q6vBjd7etl7zbwg8",
+    authDomain: "firechat-26710.firebaseapp.com",
+    databaseURL: "https://firechat-26710.firebaseio.com",
+    projectId: "firechat-26710",
+    storageBucket: "firechat-26710.appspot.com",
+    messagingSenderId: "702585727045",
+    appId: "1:702585727045:web:7682b98bb3032f56845b2c",
+    measurementId: "G-QPBB38QT68"
+  }
 };
