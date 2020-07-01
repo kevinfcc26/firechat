769861477d493d7214f20b9f487001d396a91e31
kevinfcc26 <kevinfcc26@gmail.com>
2020-07-01 16:51:15 -0500

    first commit

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../providers/chat.service';
+
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html'
+})
+export class ChatComponent implements OnInit {
+
+  mensaje: string = '';
+  elemento: any;
+
+  constructor( public _cs: ChatService ) {
+    this._cs.cargarMensajes()
+            .subscribe( ()=>{
+              setTimeout( ()=>{
+                this.elemento.scrollTop = this.elemento.scrollHeight;
+              },20 );
+            } );
+   }
+  ngOnInit(){
+    this.elemento = document.getElementById('app-mensajes');
+  }
+
+  enviarMensaje(){
+    console.log( this.mensaje );
+    if( this.mensaje.length === 0 ){
+      return;
+    }
+    this._cs.agregarMensaje( this.mensaje )
+            .then( () => this.mensaje = '' )
+            .catch( ( err ) => console.log('Error al enviar', err) );
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJgXiTaMSd5tS6pp2Q6vBjd7etl7zbwg8",
+    authDomain: "firechat-26710.firebaseapp.com",
+    databaseURL: "https://firechat-26710.firebaseio.com",
+    projectId: "firechat-26710",
+    storageBucket: "firechat-26710.appspot.com",
+    messagingSenderId: "702585727045",
+    appId: "1:702585727045:web:7682b98bb3032f56845b2c",
+    measurementId: "G-QPBB38QT68"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
