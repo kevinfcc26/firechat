78ca607389b2cd21153de4898b800672e32d53b5
kevinfcc26 <kevinfcc26@gmail.com>
2020-07-02 11:37:18 -0500

    se cambia la capeta donde se encuentra el index

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.use(express.static(__dirname+'/dist/firechat'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/firechat/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDJgXiTaMSd5tS6pp2Q6vBjd7etl7zbwg8",
     authDomain: "firechat-26710.firebaseapp.com",
